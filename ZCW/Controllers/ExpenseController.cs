93df83e785a2decf78fefa385e2a0f69a6be78be
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-10 21:40:57 +0530

    ZCW-1010 Added expense controller

---
+++ ZCW/Controllers/ExpenseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZCW.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        // GET: Expense
+        public ActionResult Index()
+        {
+            //US lead demo
+            var expenseID = getID();
+            return View();
+        }
+
+        private object getID()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
