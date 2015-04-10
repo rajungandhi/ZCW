7d9da29bd8a18bbb35b6936c699a8addc69a8acd
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-10 16:12:35 +0530

    Added Login Class

---
+++ ZCW/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZCW.Models
+{
+    public class Login
+    {
+        //15.0 changes ZCW-1001
+    }
+}
