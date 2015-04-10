c04d9be92aa74a2d2e9c483ec8c66dcda1b3b44e
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-10 15:13:09 +0530

    ZCW-1003 Added ServiceHost project

---
+++ Zerochaos.ServiceHost/IService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zerochaos.ServiceHost
+{
+    public interface IService
+    {
+    }
+}
---
+++ Zerochaos.ServiceHost/Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zerochaos.ServiceHost
+{
+    public class Service
+    {
+        //Shelve changes
+    }
+}
