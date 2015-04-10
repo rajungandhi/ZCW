d66d29c060806eb19b0499c8fb7beaf6468ab1e5
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-10 17:29:32 +0530

    ZCW-1001 Add classes

---
+++ ZCW/Models/Requisition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZCW.Models
+{
+    public class Requisition
+    {
+        public int RequisitionID { get; set; }
+        //15.0 changes
+    }
+}
---
+++ ZCW/Models/Timesheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZCW.Models
+{
+    public class Timesheet
+    {
+        public Timesheet()
+        {
+
+        }
+        public int TimesheetID { get; set; }
+    }
+}
